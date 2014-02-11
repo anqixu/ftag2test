db17455e8d07ce6d78740c4121c3ddd7e779913c
Anqi Xu <anqixu@cim.mcgill.ca>
2014-02-11 18:31:35 -0500

    first commmit

---
+++ nodes/testbench_controller.py
@@ -0,0 +1,42 @@
+#! /usr/bin/env python
+import rospy
+from std_msgs.msg import String
+
+import sys
+import getopt
+
+
+class TestbenchController():
+  def __init__(self):
+    self.alive = False
+    # TODO
+    
+  def spin(self):
+    rospy.spin()
+
+
+class Usage(Exception):
+    def __init__(self, msg):
+        self.msg = msg
+
+
+def main(argv=None):
+    if argv is None:
+        argv = sys.argv
+    try:
+        try:
+            opts, args = getopt.getopt(argv[1:], "h", ["help"])
+        except getopt.error, msg:
+             raise Usage(msg)
+             
+        controller = TestbenchController()
+        controller.spin()
+
+    except Usage, err:
+        print >>sys.stderr, err.msg
+        print >>sys.stderr, "for help use --help"
+        return 2
+
+
+if __name__ == "__main__":
+    sys.exit(main())
