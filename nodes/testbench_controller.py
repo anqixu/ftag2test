88078264f0a3b37ee0d788f57f1a473c0e301b03
Anqi Xu <anqixu@cim.mcgill.ca>
2014-02-11 19:53:32 -0500

    editing

--- nodes/testbench_controller.py
+++ nodes/testbench_controller.py
@@ -1,7 +1,8 @@
 #! /usr/bin/env python
 import rospy
-from std_msgs.msg import String
+from ftag2test.msg import ControllerState, DisplayTag
 
+import random
 import sys
 import getopt
 
@@ -19,7 +20,7 @@ class TestbenchController():
   # - Select grid-sampled pan angle, grid-sampled tilt angle (#)
   # - Requests pan-tilt unit to re-position
   # - Sleep for a bit
-  # - Select random tag name, grid-sampled rotation angle (*)
+  # - Select random tag name, random rotation angle (*)
   # - Request displayer to display random tag name
   # - Wait for displayer to respond
   # - Wait for first detection from freq_testbench
@@ -33,29 +34,39 @@ class TestbenchController():
   def __init__(self):
     self.alive = False
     self.num_detections = 0
+    self.num_images = 0
     self.num_poses = 0
     self.fsm = State.IDLE
     self.curr_pose = None
+    self.curr_image = DisplayTag()
     
     self.tagImages = ['ftag2_6S5F3B_40000_00000_40000_00000_40000_00000.png', 'ftag2_6S5F3B_50000_00000_40000_00000_40000_00000.png'] # TODO: load list of images
+    self.permutedImages = [0, 1] # TODO: permute randomly
     self.PTAngles = [(0, 0), (0, 5), (0, 10)] # TODO: load list of poses
+
+    # TODO: subscribe to displayer ack callback
+    
+    # TODO: subscribe to freq_testench callback
     
     # TODO: open serial interface to arduino
-    
+  
     # TODO: spawn user keyboard command thread
 
-
+    
   def reset(self):
+    self.permutedImages = [0, 1] # TODO: permute randomly
     self.num_detections = 0
+    self.num_images = 0
     self.num_poses = 0
     self.curr_pose = None
+    self.curr_image = DisplayTag()
     self.fsm = State.REPOSITION_PAN_TILT
     self.alive = True
 
     
   def spin(self):
     while not rospy.is_shutdown():
-      if self.fsm == State.IDLE:
+      if self.fsm == State.IDLE or self.fsm == State.WAIT_FOR_DISPLAYER:
         rospy.sleep(0.00001)
         
       elif self.fsm == State.REPOSITION_PAN_TILT:
@@ -67,11 +78,23 @@ class TestbenchController():
         self.fsm = State.REQUEST_DISPLAYER
         
       elif self.fsm == State.REQUEST_DISPLAYER:
+        idx = self.permutedImages[self.num_images % len(self.permutedImages)]
+        self.curr_image.filename = self.tagImages[idx]
+        self.curr_image.rotation = random.random() * 360.0
+        self.num_images += 1
         
+        # TODO: publish request to displayer
+
+        self.fsm = State.WAIT_FOR_DISPLAYER
+
+      elif 
         
-        # TODO: send message requesting displayer
-        
-        
+  # - Wait for first detection from freq_testbench
+  # - Publish encoded tag phases, along with frame ID of first detection
+  # - Wait for N detections
+  # - Publishes empty, along with frame ID of last accepted detection
+  # - Return to (*) if haven't reached T iterations, else return to (#); and if all poses have been iterated over, then idle
+
 
         
 
