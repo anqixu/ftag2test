bbcec516bc65adf639cbc7c13deb0914d265c105
MiMiC <mimic@mimic-mbp-ubuntu.(none)>
2014-02-12 19:24:47 -0400

    extended sleep time after image changes

--- nodes/testbench_controller.py
+++ nodes/testbench_controller.py
@@ -167,7 +167,7 @@ class TestbenchController():
 
   def processAck(self, msg):
     if self.alive and self.fsm == State.WAIT_FOR_DISPLAYER:
-      rospy.sleep(0.3)
+      rospy.sleep(0.35)
       self.num_detections = 0
       self.latest_frame_id = -1
       if self.timeout is not None:
