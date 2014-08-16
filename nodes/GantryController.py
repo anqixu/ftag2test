b0b0297c96ef0fadf8b2d7f36da599e7299a71cc
Anqi Xu <anqixu@cim.mcgill.ca>
2014-08-16 15:54:28 -0400

    still improving gantry controller, about to test it on real machine

--- nodes/GantryController.py
+++ nodes/GantryController.py
@@ -22,7 +22,7 @@ class GantryController:
 
   
   # NOTE: max baud rate is 38400
-  def __init__(self, device='/dev/ttyS4', baud=38400, timeout=None, verbose=True, is_sim=False):
+  def __init__(self, device='/dev/ttyS4', baud=38400, timeout=None, verbose=True, is_sim=False, suicide_secs=None):
     self.alive = True
     self.gantry_initialized = False
     self.gantry = None
@@ -32,6 +32,7 @@ class GantryController:
     self.is_sim = is_sim
     self.write_queue_mutex = threading.Lock()
     self.write_queue = []
+    self.read_log = ''
     
     if not self.is_sim:
       self.gantry = serial.Serial(port=device, baudrate=baud, \
@@ -46,9 +47,18 @@ class GantryController:
       
     self.initGantry()
       
-    self.spin_thread = threading.Thread(target=self.spinLoop)
-    self.spin_thread.start()
-      
+    #self.spin_thread = threading.Thread(target=self.spinLoop)
+    #self.spin_thread.start()
+    
+    if suicide_secs is not None and suicide_secs > 0:
+      self.suicide_timer = threading.Timer(suicide_secs, self.suicide)
+      self.suicide_timer.start()
+
+
+  def suicide(self):
+    print 'Gantry controller suicided!'
+    self.alive = False
+    
       
   def __enter__(self):
     return self
@@ -59,7 +69,7 @@ class GantryController:
     
     if self.spin_thread and self.spin_thread.isAlive():
       self.spin_thread.join(5.0)
-    
+      
     try:
       if self.gantry is not None and self.gantry.isOpen():
         self.gantry_mutex.acquire()
@@ -67,6 +77,13 @@ class GantryController:
         self.gantry = None
         self.gantry_mutex.release()
         print 'Gantry connection closed'
+        
+        logfile = 'log.txt'
+        log = open(logfile, 'a')
+        log.write(self.read_log)
+        close(log)
+        print 'Wrote to log: %s' % logfile
+        
     except Exception, e:
       print 'Exception on exit: %s' % e
       pass
@@ -75,12 +92,14 @@ class GantryController:
   def _read(self, terminal_token='\r', maxlength=2000): # WARNING: NO MUTEX PROTECTIONS!
     if self.is_sim:
       return ''
+    self.read_log += '\n--^--\n'
     content = ''
     reading = True
-    while reading:
+    while reading and self.alive and self.gantry is not None:
       data = self.gantry.read()
       if data and len(data) > 0:
         #print '.', data
+        self.read_log += data
         content += data
         if data.find(terminal_token) >= 0:
           reading = False
@@ -88,6 +107,7 @@ class GantryController:
         elif len(content) > maxlength:
           reading = False
           break
+    self.read_log += '\n--v--\n'
     return content
     
 
@@ -96,10 +116,10 @@ class GantryController:
       return []
     lines = []
     reading = True
-    while reading:
-      self.gantry_mutex.acquire()
+    self.read_log += '\n==^==\n'
+    while reading and self.alive and self.gantry is not None:
       line = self.gantry.readline()
-      self.gantry_mutex.release()
+      self.read_log += line + '\n'
       if line and len(line) > 0:
         lines.append(line)
         if len(lines) > maxlines:
@@ -108,6 +128,7 @@ class GantryController:
       else:
         reading = False
         break
+    self.read_log += '\n==v==\n'
     return lines
 
 
@@ -136,12 +157,10 @@ class GantryController:
           self.write_queue_mutex.release()
           for cmd in cmds:
             self.gantry_mutex.acquire() # TODO: why is this loop locking?
-            if self.gantry is None or not self.alive:
-              self.gantry_mutex.release()
-              self.alive = False
-              return
-            self.gantry._write(cmd)
+            self._write(cmd)
             self.gantry_mutex.release()
+            if not self.alive:
+              break
         time.sleep(0.001)
         
     else: # For real robot, continue to query W1 states until write_queue has content, then flush and repeat
@@ -151,66 +170,65 @@ class GantryController:
         # Initiate a W1 repeated poll request
         if not parsing_W1:
           self.gantry_mutex.acquire()
-          if self.gantry is None or not self.alive:
-            self.gantry_mutex.release()
-            self.alive = False
-            return
           self.gantry._write('W1\r')
           self.gantry._read(maxlength=50) # Skip header for W1
           parsing_W1 = True
           self.write_queue_mutex.release()
 
+        if not self.alive:
+          break
+
         # Obtain one W1 state update from robot
         self.gantry_mutex.acquire()
-        if self.gantry is None or not self.alive:
-          self.gantry_mutex.release()
-          self.alive = False
-          return
-        state = self.gantry._read(terminal_token='\r')
-        print 'state:', state # TODO: parse state and do something with it, e.g. callback fn
+        if self.gantry is not None:
+          state = self.gantry._read(terminal_token='\r')
+          print 'state:', state # TODO: parse state and do something with it, e.g. callback fn
         self.write_queue_mutex.release()
+
+        if not self.alive:
+          break
         
         # Flush write queue if needed
         if len(self.write_queue) > 0:
           # First stop W1 queries
           self.gantry_mutex.acquire()
-          if self.gantry is None or not self.alive:
-            self.gantry_mutex.release()
-            self.alive = False
-            return
-          self.gantry._write('\r')
-          self.gantry._read(terminal_token='\r')
-          parsing_W1 = False
+          if self.gantry is not None:
+            self.gantry._write('\r')
+            self.gantry._read(terminal_token='\r')
+            parsing_W1 = False
           self.write_queue_mutex.release()
-          
+
           # Then dump write queue
           self.write_queue_mutex.acquire()
           cmds = self.write_queue
           self.write_queue = []
           self.write_queue_mutex.release()
+
+          if not self.alive:
+            break
           
           # Now sequentially issue commands
           for cmd in cmds:
             self.gantry_mutex.acquire()
-            if self.gantry is None or not self.alive:
-              self.gantry_mutex.release()
-              self.alive = False
-              return
-            self.gantry._write(cmd)
+            if self.gantry is not None:
+              self.gantry._write(cmd)
             self.gantry_mutex.release()
+            if not self.alive:
+              break
           
           
   def initGantry(self, calibrate=True, calibrate_when_not_homed=False):
     self.gantry_mutex.acquire()
     
     self._write('NOHELP\r') # disables auto-complete
-    self._readlines(1) # TODO: should we read 2+ lines instead?
+    print '<<<', self._readlines(1), '\n' # TODO: should we read 2+ lines instead?
     self._write('ONPOWER\r') # blocks until motors come on
-    self._readlines(1) # TODO: should we read 2+ lines instead?
+    print '<<<', self._readlines(1), '\n' # TODO: should we read 2+ lines instead?
     
     if calibrate_when_not_homed:
       self._write('STATUS\r')
       lines = self._readlines() # TODO: how many lines?
+      print '<<<', lines, '\n' # TODO: remove
       for line in lines:
         if line.find('NOT HOMED') >= 0:
           calibrate = True
@@ -218,12 +236,13 @@ class GantryController:
     if calibrate:
       print 'Calibrating...'
       self._write('RUN CAL\r')
-      self._readlines(1) # TODO: should we read 2+ lines instead?
+      print '<<<', self._readlines(1), '\n' # TODO: should we read 2+ lines instead?
       self._write('@ZERO\r')
-      self._readlines(1) # TODO: should we read 2+ lines instead?
+      print '<<<', self._readlines(1), '\n' # TODO: should we read 2+ lines instead?
       # TODO: block until calibration returns
+
     self._write('RUN INIT_SYS\r') # initialize gantry data, e.g. units
-    self._readlines(1) # TODO: should we read 2+ lines instead?
+    print '<<<', self._readlines(1), '\n' # TODO: should we read 2+ lines instead?
     
     self.gantry_mutex.release()
     
@@ -275,19 +294,19 @@ def main():
   parser = argparse.ArgumentParser(description='GantryController Tester')
   parser.add_argument("-d", "--device", type=str, required=False, help="Path to serial device", default='/dev/ttyS4')
   parser.add_argument('-s', '--sim', required=False, help="Simulate commands without connecting to serial device", action='store_true')
+  parser.add_argument('-k', '--suicide', type=int, required=False, help="Number of seconds to run before gantry controller suicides", default=None)
   args = parser.parse_args()
   
-  # TODO: have a timer kill this program after N minutes
-  
-  with GantryController(args.device, is_sim = args.sim) as gantry:
+  with GantryController(args.device, is_sim=args.sim, suicide_secs=args.suicide) as gantry:
     if args.sim:
       print 'Writing stuff'
       gantry.write('SPEED 40\r')
       gantry.translateRel(100, 200, 300)
       gantry.translateRel(-111.1, -222.2, 333.3, True)
       
-      time.sleep(10.0)
-      gantry.alive = False
+      print 'Waiting for controller to self terminate'
+      while gantry.alive:
+        time.sleep(0.001)
       
     else:
       gantry.write('SPEED 40\r')
