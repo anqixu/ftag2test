a6516df6ce0cc2045358000109833b34dc5f601c
Anqi Xu <anqixu@cim.mcgill.ca>
2015-04-09 17:14:04 -0400

    added debug nodes for displaying rotational angles (in degrees) for different types of tags

---
+++ nodes/printDecodedFTag2Quat2Rot.py
@@ -0,0 +1,35 @@
+#! /usr/bin/env python
+
+import rospy
+from ftag2_core.msg import TagDetections
+import tf
+import math
+
+
+class FTag2Quat2RotNode:
+  def __init__(self):
+    rospy.init_node('ftag2_quat_2_rxyz_node')
+    self.sub_tags = rospy.Subscriber('/ftag2/detected_tags', TagDetections, self.processTagsMsg)
+    rospy.loginfo('%s initialized' % rospy.get_name())
+    
+   
+  def processTagsMsg(self, msg):
+    rospy.loginfo('Detected %d tags: (rx_deg, ry_deg, rz_deg, x_m, y_m, z_m, ID)' % len(msg.tags))
+    count = 0
+    for tag in msg.tags:
+      count += 1
+      rxyz = tf.transformations.euler_from_quaternion([tag.pose.orientation.x, tag.pose.orientation.y, tag.pose.orientation.z, tag.pose.orientation.w])
+      rospy.loginfo('  %d: %.2f, %.2f, %.2f   (%.2f, %.2f, %.2f)   %s' % (count, math.degrees(rxyz[0]), math.degrees(rxyz[1]), math.degrees(rxyz[2]), tag.pose.position.x, tag.pose.position.y, tag.pose.position.z, tag.decodedPayloadStr))
+    rospy.loginfo('')
+
+    
+  def spin(self):
+    rospy.spin()
+
+
+if __name__ == "__main__":
+  try:
+    node = FTag2Quat2RotNode()
+    node.spin()
+  except rospy.ROSInterruptException:
+    pass
